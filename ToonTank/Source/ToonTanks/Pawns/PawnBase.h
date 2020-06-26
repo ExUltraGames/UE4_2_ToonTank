c69df9c29a828666493577ce29ead1b5e24bce29
chineseburn <chineseburn@gmail.com>
2020-06-26 17:54:34 +0100

    135: Creating Components

--- ToonTank/Source/ToonTanks/Pawns/PawnBase.h
+++ ToonTank/Source/ToonTanks/Pawns/PawnBase.h
@@ -6,24 +6,37 @@
 #include "GameFramework/Pawn.h"
 #include "PawnBase.generated.h"
 
+//Forward Declare
+class UCapsuleComponent;
+
 UCLASS()
 class TOONTANKS_API APawnBase : public APawn
 {
 	GENERATED_BODY()
 
+private:
+	//Universal components we will need // NEED TO BE UPROPERTY
+	UPROPERTY()
+	UCapsuleComponent* CapsuleComp; 
+	UPROPERTY()
+	UStaticMeshComponent* BaseMesh;
+	UPROPERTY()
+	UStaticMeshComponent* TurretMesh;
+	UPROPERTY()
+	USceneComponent* ProjectileSpawnPoint;
+
 public:
 	// Sets default values for this pawn's properties
 	APawnBase();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
 };
